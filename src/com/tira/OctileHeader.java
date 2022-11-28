c7b7cf3934e71244210688b4b49aae12f5e35d78
Sofia Riekkola <sofiariekkola@Sofia-MBP-2.home>
2022-11-28 09:53:22 +0200

    Add octile header

---
+++ src/com/tira/OctileHeader.java
@@ -0,0 +1,43 @@
+package com.tira;
+
+import java.util.List;
+
+public class OctileHeader implements IMapHeader{
+    private boolean isoctile = false;
+    private int height;
+    private int width;
+    public int getHeight() {
+        return height;
+    }
+
+    @Override
+    public boolean isValidHeader(List<String> hdrLines) {
+        if (hdrLines.get(0).compareTo("type octile") == 0 &&
+                hdrLines.get(3).compareTo("map") == 0
+        ) {
+            System.out.println("This is octile header");
+            this.isoctile = true;
+            // read the dimensions
+            String[] dLine = hdrLines.get(1).split(" ");
+            if (dLine[0].compareTo("height") == 0) {
+                this.height = Integer.valueOf(dLine[1]);
+            }
+            dLine = hdrLines.get(2).split(" ");
+            if (dLine[0].compareTo("width") == 0) {
+                this.width = Integer.valueOf(dLine[1]);
+            }
+        }
+        return isoctile;
+    }
+
+    @Override
+    public int headerLines() {
+        return 4;   // octile header uses 4 lines in the file
+    }
+
+    public int getWidth() {
+        return width;
+    }
+    public OctileHeader() {
+    }
+}
