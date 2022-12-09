cea411e800f83cd4e0020ea54b73a9c5ccb74909
Sofia Riekkola <sofiariekkola@Sofia-MBP-2.home>
2022-12-09 19:44:23 +0200

    Expand gridnode

--- src/main/java/com/tira/GridNode.java
+++ src/main/java/com/tira/GridNode.java
@@ -4,10 +4,23 @@ public class GridNode {
     private final int x;
     private final int y;
     private NodeType t;
+
+    private boolean searching = false;
+    private boolean checked = false;
     public GridNode(int x, int y, NodeType t) {
         this.x = x;
         this.y = y;
         this.t = t;
+        this.searching = false;
+        this.checked = false;
+    }
+
+    public GridNode(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.t = NodeType.FREE;
+        this.searching = false;
+        this.checked = false;
     }
 
     public void setNodeType(NodeType t) {
@@ -16,4 +29,41 @@ public class GridNode {
     public  NodeType getNodeType() {
         return this.t;
     }
+
+    public int getX() {
+        return x ;
+    }
+    public int getY() {
+        return y ;
+    }
+
+    public String toString() {
+        return "("+x+","+y+"):"+t+ " "+(searching? "!": " ");
+    }
+    public boolean isSearching() {
+        return searching;
+    }
+
+    public void setSearching(boolean searching) {
+        this.searching = searching;
+    }
+
+    public boolean isChecked() {
+        return checked;
+    }
+
+    public void setChecked(boolean checked) {
+        this.checked = checked;
+    }
+
+    // Return true in comparison if another GridNode has same coordinates
+    public boolean equals(Object n) {
+        if (n instanceof GridNode) {
+            if (x==((GridNode)n).getX() && y == ((GridNode)n).getY()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
