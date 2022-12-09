271a284aef0c38d97999a0f474c39646e5ab385f
Sofia Riekkola <sofiariekkola@Sofia-MBP-2.home>
2022-12-09 17:59:21 +0200

    Add diagonal heuristic

---
+++ src/main/java/com/tira/DiagonalHeuristic.java
@@ -0,0 +1,34 @@
+package com.tira;
+
+// Used http://theory.stanford.edu/~amitp/GameProgramming/Heuristics.html#diagonal-distance
+// for inspiration
+public class DiagonalHeuristic implements IHeuristic {
+    Double D = 1.5;
+    Double D2 = 1.5;
+
+    @Override
+    public Double heuristic(GridNode n1, GridNode n2) {
+        int dx = Math.abs(n1.getX() - n2.getX());
+        int dy = Math.abs(n1.getY() - n2.getY());
+        if (dx > dy) {
+            return (D * (dx-dy) + D2 * dy);
+        } else {
+            return (D * (dy-dx) + D2 * dx);
+        }
+    }
+
+    @Override
+    public Double cost(GridNode n1, GridNode n2) {
+        int dx = Math.abs(n1.getX() - n2.getX());
+        int dy = Math.abs(n1.getY() - n2.getY());
+        return Double.valueOf(dx+dy);
+    }
+
+    public void setD(Double d) {
+        D = d;
+    }
+
+    public void setD2(Double d) {
+        D2 = d;
+    }
+}
